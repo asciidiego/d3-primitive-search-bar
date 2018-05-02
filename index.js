5de01a8cd9338d829ab88bb62c4a4ad1405c8d87
diego <diego.vincent.rodriguez@gmail.com>
2018-05-02 23:14:49 +0200

    Changed labels and restored the 3D Translation when user mouse leaves the area.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var typed = new Typed('#title', {
-  // Waits 1000ms after typing "First"
-  strings: ["Google. ^1000 Made simpler."]
+    // Waits 1000ms after typing "First"
+    strings: ["Google. ^1000 Made simpler."]
 });
+
+let search_box = d3.select('#main-search');
+
+search_box.on('mouseover', function () {
+    d3.select('#parallax-div')
+        .style("transform", "translateZ(2em)");
+    })
+    .on('mouseout', function () {
+        d3.select('#parallax-div')
+            .style("transform", "translateZ(0)");
+    });
